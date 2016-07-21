b4fbd218a6902ce8384f2882c9f015aafe076744
Zach Kusz <zskusz@gmail.com>
2016-07-21 18:43:54 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Mini Lego
+---
+  This is primarily a learning app. My goals are to learn and implement
+  1. express-handlebars
+  2. grunt (or gulp)
+  3. proper documentation for once
+  4. passport
+
+##The App
+  The Mini Lego app is designed to aid in the creation of "mini" or "micro"
+  lego sets by allowing users to track their collection of individual lego
+  pieces and inspire creations with images of featured creations.
+
+##Built With
+  1. node
+  2. express
+  3. mongod
+  4. ...
+
+##Instalation
+  Install using npm:
+  '''$ npm install express
+  npm install express-handlebars
+  '''
+
+##Versions
+  * 1.0.0 : Not yet at a working version
+
+##Authors
+  * Zach Kusz
---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var exphbs  = require('express-handlebars');
+
+//handlebar stuffs
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('index'); //automaticly looks for .handlebars extension
+});
+
+// Serve back static files
+//app.use(express.static(path.join(__dirname, '/public'))); primes way
+app.use('/public', express.static('public')); //tutorial way
+
+//set port and listen
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), function() {
+  console.log('Listening on port: ', app.get('port'));
+});
