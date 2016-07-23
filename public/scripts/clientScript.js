7367a8d4cea1da6cb7ab90e8ad0902f9a058c64c
Zach Kusz <zskusz@gmail.com>
2016-07-23 17:58:15 -0500

    includes angular, sets angular to work with handlebars

--- public/scripts/clientScript.js
+++ public/scripts/clientScript.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('ayyy');
+var app = angular.module('legoApp', []);
+
+app.config(function($interpolateProvider) {
+  $interpolateProvider.startSymbol('{[{');
+  $interpolateProvider.endSymbol('}]}');
+});
+
+app.controller('mainController', ['$scope','$http', '$window',
+function($scope, $http, $window) {
+
+  var pie = 1;
+  console.log('pie', pie);
+  $scope.hi = 'hello from angular';
+  $scope.add = function() {
+    pie++;
+    console.log('pie', pie);
+    return;
+  }
+
+}]);
