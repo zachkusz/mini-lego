4bacb4660595546db176cea595378f6e1f44dbe9
Zach Kusz <zskusz@gmail.com>
2016-07-24 19:11:20 -0500

    fixes directory pathing

--- app.js
+++ app.js
@@ -8,21 +8,26 @@ var mongoose = require('mongoose'); //youtube guy also required mongodb why?
 
 var app = express();
 
-//handlebar stuffs
+//handlebar view engine
+app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+//body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Serve back static files
+app.use(express.static(path.join(__dirname, 'public')));// primes way
+//app.use('/public', express.static('public')); //tutorial way
+
 app.get('/', function (req, res) {
-    res.render('index'); //automaticly looks for .handlebars extension
+  res.render('index'); //automaticly looks for .handlebars extension
 });
 
 app.get('/browse/', function (req, res) {
   res.render('browse');
 });
-
-// Serve back static files
-//app.use(express.static(path.join(__dirname, '/public')));// primes way
-app.use('/public', express.static('public')); //tutorial way
 
 //set port and listen
 app.set('port', process.env.PORT || 3000);
